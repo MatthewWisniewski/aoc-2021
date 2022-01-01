6b254142f69a3a3238c5f6ee961060bce652f1c0
Clevershelf009 <matthewski490@yahoo.co.uk>
2022-01-01 20:29:55 +0000

    Completed day 4

---
+++ day_4/4-2.jl
@@ -0,0 +1,104 @@
+mutable struct BingoBoard
+    board :: Vector{Vector{Int}}
+    markedBoard :: Vector{Vector{Bool}}
+end
+
+function markBoardIfNumberPresent!(board :: BingoBoard, n :: Int)
+    for row in range(1, length(board.board))
+        for col in range(1, length(board.board[1]))
+            if board.board[row][col] == n
+                board.markedBoard[row][col] = true
+            end
+        end
+    end
+end
+
+function hasBingoed(board :: BingoBoard)
+
+    # Check if any rows are complete
+    for row in range(1, length(board.markedBoard))
+        bingo = true
+        for col in range(1, length(board.markedBoard[1]))
+            if board.markedBoard[row][col] == false
+                bingo = false
+                break
+            end
+        end
+        if bingo
+            return true
+        end
+    end
+
+    # Check if any columns are complete
+    for col in range(1, length(board.markedBoard[1]))
+        bingo = true
+        for row in range(1, length(board.markedBoard))
+            if board.markedBoard[row][col] == false
+                bingo = false
+                break
+            end
+        end
+        if bingo
+            return true
+        end
+    end
+    return false
+end
+
+function sumUnmarkedValues(board :: BingoBoard)
+    total = 0
+    for row in range(1, length(board.board))
+        for col in range(1, length(board.board[1]))
+            if board.markedBoard[row][col] == false
+                total += board.board[row][col]
+            end
+        end
+    end
+    return total
+end
+
+function day_4_part_2(boards :: Vector{BingoBoard}, numbersCalled :: Vector{Int})
+    mostRecentScore = 0
+    for number in numbersCalled
+        indicesToDelete = Vector{Int}()
+        for (i, board) in enumerate(boards)
+            markBoardIfNumberPresent!(board, number)
+            if (hasBingoed(board))
+                mostRecentScore =  sumUnmarkedValues(board) * number
+                push!(indicesToDelete, i)
+            end
+        end
+        deleteat!(boards, indicesToDelete)
+        if (length(boards) == 0)
+            return mostRecentScore
+        end
+    end
+end
+
+## MAIN PROGRAM
+
+lines = readlines(open("day 4 input.txt"))
+
+numbersCalled = map(x -> parse(Int, x), split(lines[2], ","))
+boards = Vector{BingoBoard}()
+
+
+for lineNo in range(4, length(lines)-6, step=6)
+    gameBoard = Vector{Vector{Int}}(undef, 5)
+    for i in range(0, 4)
+        gameBoard[i + 1] = map(x-> parse(Int, x), split(lines[lineNo + i]))
+    end
+
+    markedBoard = Vector{Vector{Bool}}(undef, 5)
+    for i in range(1, 5)
+        markedBoard[i] = Vector{Bool}(undef, 5)
+        for j in range(1, length(5))
+            markedBoard[i][j] = false
+        end
+    end
+
+    push!(boards, BingoBoard(gameBoard, markedBoard))
+end
+
+
+println(day_4_part_2(boards, numbersCalled))
